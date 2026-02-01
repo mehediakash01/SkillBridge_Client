52ebb9941b74896a1ae8beface80441afb4e0828
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-02-02 02:20:28 +0600

    feat(auth):intialized email verification with nodemailer

--- lib/app.ts
+++ lib/app.ts
@@ -1,8 +1,17 @@
 import { toNodeHandler } from "better-auth/node";
 import express, { Application } from "express"
+import cors from "cors"
 import { auth } from "./auth.js";
 const app:Application = express()
+
 app.use(express.json())
+app.use(cors({
+  origin:process.env.APP_URL ||"http://localhost:5000",
+    credentials:true
+}
+  
+   
+))
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/',(req,res)=>{
